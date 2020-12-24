07b4ca627de92d995a59415a01b4c322c0676b1b
Namory Traore <namorytraore1984@yahoo.fr>
2020-12-23 21:22:53 -0500

    travaux encours

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarreSuppComponent } from './barre-supp/barre-supp.component';
 import { LogoComponent } from './logo/logo.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 
 @NgModule({
-  declarations: [BarreSuppComponent, LogoComponent],
+  declarations: [BarreSuppComponent, LogoComponent, NavBarComponent],
   imports: [
     CommonModule
   ],
-  exports: [BarreSuppComponent, LogoComponent]
+  exports: [BarreSuppComponent, LogoComponent, NavBarComponent]
 })
 export class CoreModule { }
